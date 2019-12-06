32ac8e4127293c8858fae2b37adbf4d61ac166c3
Dariusz Syncerek <dsyncerek@outlook.com>
2019-12-06 22:03:27 +0100

    Add ServeStatic to serve client app in prod

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,19 @@
 import { ClassSerializerInterceptor, Module, ValidationPipe } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { ArticleModule } from './article/article.module';
 import { BlogModule } from './blog/blog.module';
 import { CategoryModule } from './category/category.module';
 import { AllExceptionsFilter } from './common/all-exceptions.filter';
 import { getTypeOrmConfig } from './config/typeorm.config';
 
+const clientBuildPath = join(__dirname, '../..', 'client/build');
+
 @Module({
   imports: [
+    ServeStaticModule.forRoot({ rootPath: clientBuildPath }),
     TypeOrmModule.forRootAsync({ useFactory: async () => getTypeOrmConfig() }),
     ArticleModule,
     BlogModule,
