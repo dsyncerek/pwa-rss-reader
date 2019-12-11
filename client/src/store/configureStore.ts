8517a309c37012d0532b726f064b8bb7a01ee526
Dariusz Syncerek <dsyncerek@outlook.com>
2019-12-11 13:08:48 +0100

    Add missing deps

--- client/src/store/configureStore.ts
+++ client/src/store/configureStore.ts
@@ -1,8 +1,9 @@
 import { applyMiddleware, compose, createStore, Store } from 'redux';
+import thunk from 'redux-thunk';
 import { callApiMiddleware } from '../middlewares/callApiMiddleware';
 import { rootReducer } from '../reducers';
 
 export function configureStore(): Store {
   const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  return createStore(rootReducer, composeEnhancers(applyMiddleware(callApiMiddleware())));
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, callApiMiddleware())));
 }
