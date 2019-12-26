4632237764ff1992893489b8e4a458a67a85b8dc
Dariusz Syncerek <dsyncerek@outlook.com>
2019-12-26 18:07:23 +0100

    Add sw logging

--- client/src/sw/onFetch.js
+++ client/src/sw/onFetch.js
@@ -9,25 +9,30 @@ export async function onFetch(event) {
   const { url, method, mode } = request;
 
   if (method !== 'GET') {
+    console.log('GET, networkOnly', request.url);
     event.respondWith(networkOnly(request));
     // event.respondWith(networkOnlyWithBackgroundSync(request)); // todo
     return;
   }
 
   if (STATIC_ASSETS.includes(url)) {
+    console.log('Static asset, cacheFirst', request.url);
     event.respondWith(cacheFirst(request, { cacheName: CACHES.STATIC }));
     return;
   }
 
   if (url.match(API_URL_REGEX)) {
+    console.log('API call, networkFirst', request.url);
     event.respondWith(networkFirst(request, { cacheName: CACHES.API }));
     return;
   }
 
   if (mode === 'navigate' && !url.match(FILE_URL_REGEX)) {
+    console.log('Navigate, staleWhileRevalidate', request.url);
     event.respondWith(staleWhileRevalidate(APP_ENTRYPOINT, { cacheName: CACHES.STATIC }));
     return;
   }
 
+  console.log('Other, staleWhileRevalidate', request.url);
   event.respondWith(staleWhileRevalidate(request, { cacheName: CACHES.RUNTIME }));
 }
