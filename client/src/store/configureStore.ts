a9ce33c56261770333c9b4031df94698ecde2faf
Dariusz Syncerek <dsyncerek@outlook.com>
2020-02-11 20:21:13 +0100

    refactor: remove redux-logger

--- client/src/store/configureStore.ts
+++ client/src/store/configureStore.ts
@@ -1,14 +1,11 @@
 import { applyMiddleware, createStore, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import { initEntitiesFromIndexedDb } from '../common/entity/entityActions';
 import { rootReducer } from './reducers';
 
-const logger = createLogger({ collapsed: true });
-
 export async function configureStore(): Promise<Store> {
-  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
+  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
   try {
     await initEntitiesFromIndexedDb()(store.dispatch, store.getState, null);
